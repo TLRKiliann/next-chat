e3b77ac3614f52680bbeeb47fe102a677ed404ea
TLRKiliann <philogenie@protonmail.com>
2024-02-19 14:32:03 +0100

    fake lib created

---
+++ my-app/app/lib/data.ts
@@ -0,0 +1,44 @@
+import type { UsersProps } from './definitions';
+/* import devola from "@/public/next.svg";
+import devola from "@/public/next.svg";
+import devola from "@/public/next.svg";
+import devola from "@/public/next.svg";
+import devola from "@/public/next.svg"; */
+
+export const userschat: UsersProps[] = [
+    {
+        id: 1,
+        img: "",
+        username: "Devola",
+        email: "devola@mail.com",
+        onLine: true
+    },
+    {
+        id: 2,
+        img: "",
+        username: "Mike",
+        email: "mike@mail.com",
+        onLine: true
+    },
+    {
+        id: 3,
+        img: "",
+        username: "Doug",
+        email: "doug@mail.com",
+        onLine: true
+    },
+    {
+        id: 4,
+        img: "",
+        username: "Magalie",
+        email: "maga@mail.com",
+        onLine: false
+    },
+    {
+        id: 5,
+        img: "",
+        username: "Aglianica",
+        email: "aglianica@mail.com",
+        onLine: true
+    }
+]
---
+++ my-app/app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type UsersProps = {
+    id: number;
+    img: string;
+    username: string;
+    email: string;
+    onLine: boolean;
+};
