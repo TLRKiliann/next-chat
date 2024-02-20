50862d9784e1c354524a58795a46893795f725d7
TLRKiliann <philogenie@protonmail.com>
2024-02-20 13:32:22 +0100

    ui improved

--- my-app/app/(auth)/logout/page.tsx
+++ my-app/app/(auth)/logout/page.tsx
@@ -12,7 +12,7 @@ export default function LogoutPage() {
 
   if (session) {
     return (
-      <div className="w-full h-screen flex flex-col justify-center items-center">
+      <div className="w-full h-screen flex flex-col justify-center items-center bg-slate-800">
         <div className="w-44 h-44 relative mb-4">
           <Image
               src={session.user?.image as string}
@@ -21,7 +21,7 @@ export default function LogoutPage() {
               className="object-cover rounded-full"
           />
         </div>
-        <p className="text-2xl mb-2">Welcome 
+        <p className="text-2xl mb-2">Welcome&nbsp;
         <span className="font-bold">
           {session.user?.name}
         </span>. Signed In As</p>
@@ -29,19 +29,13 @@ export default function LogoutPage() {
           {session.user?.email}
         </p>
         <button className="text-slate-50 bg-red-600/90 transition trasform duration-200 
-          hover:scale-105 py-2 px-4 rounded-md hover:shadow-btn" 
+          hover:scale-105 py-2 px-6 rounded-md hover:shadow-btn" 
           onClick={() => signOut()}>
           Sign out
         </button>
         <li className="text-lg font-bold text-blue-500 hover:text-blue-600 active:text-blue-400 mt-4">
-          <Link href="/products">
-            Go to products
-          </Link>
-        </li>
-
-        <li className="text-lg font-bold text-blue-500 hover:text-blue-600 active:text-blue-400">
-          <Link href="/about">
-            Go to about
+          <Link href="/chatroom">
+            Return to chat
           </Link>
         </li>
 
