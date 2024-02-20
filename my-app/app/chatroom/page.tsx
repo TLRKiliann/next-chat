7e4ff03e8202beca6b1e0af4d5f491eaba2fdfd3
TLRKiliann <philogenie@protonmail.com>
2024-02-20 09:22:22 +0100

    ui improved

--- my-app/app/chatroom/page.tsx
+++ my-app/app/chatroom/page.tsx
@@ -19,27 +19,28 @@ export default function ChatRoom() {
 
     const handleClick = () => {
         setAllStr((prevState) => [...prevState, inputStr]);
+        setInputStr("");
     }
 
     return (
-        <div className='h-screen border-2'>
+        <div className='h-screen'>
             
-            <h1 className='text-2xl italic font-bold p-[20px] border'>
+            <h1 className='text-2xl italic font-bold p-[20px]'>
                 Chat room
             </h1>
 
-            <div className='flex w-full h-[calc(100%-70px)] border-2 border-orange-400'>
+            <div className='flex w-full h-[calc(100%-70px)]'>
 
-                <div className='flex flex-col w-[280px] bg-blue-900 border'>
+                <div className='flex flex-col w-[20%] bg-blue-900'>
 
                     {users.map((user: UsersProps) => (
                         user.onLine ? (
                             <div key={user.id} 
-                                className='flex items-center justify-between bg-slate-800 mb-4 px-4 py-2 border'>
+                                className='flex items-center justify-between bg-slate-800 mb-4 px-4 py-2'>
                                 <Image src={user.img} width={30} height={30} alt={user.username} 
                                     className='flex w-[30px] h-[30px] object-cover rounded-full'
                                 />
-                                <p className='w-[120px] border'>{user.username}</p>
+                                <p className='w-[120px]'>{user.username}</p>
                                 {/* <p className='w-[50px] border'>{user.email}</p> */}
                                 {user.onLine === true 
                                     ? <span className='w-[20px] h-[20px] bg-green-400 rounded-full'></span> 
@@ -52,30 +53,36 @@ export default function ChatRoom() {
                 </div>
 
 
-                <div className='w-full border'>
+                <div className='w-full'>
                     
                     <div className='flex justify-between bg-slate-700 h-[calc(100%-80px)]'>
                         
-                        <div className='border'>
+                        <div className='w-full h-full overflow-scroll scroll-smooth bg-yellow-600'>
+                        <p className='text-slate-600 bg-slate-100 m-4 p-2 rounded-tr-lg rounded-bl-lg rounded-br-lg'>
                             Left screen
+                        </p>
                         </div>
 
-                        <div className='border'>
+                        <div className='w-full h-full overflow-scroll scroll-smooth bg-blue-600'>
                             {allStr.map((a: string) => (
-                                <p>{a}</p>
+                                <p className='text-slate-600 bg-slate-100 m-4 p-2 rounded-tl-lg rounded-tr-lg rounded-bl-lg'>
+                                    {a}
+                                </p>
                             ))}
                         </div>
 
                     </div>
                     
-                    <div className='flex items-center justify-center h-[80px] border'>
+
+
+                    <div className='absolute flex items-center justify-between w-5/6 h-[80px] bg-slate-700 z-10 px-4'>
 
                         <input type="text" value={inputStr} onChange={handleChange} 
                             placeholder="Write something here..." 
-                            className='w-full text-slate-800 mx-4 px-4 py-1 rounded-full'
+                            className='w-[90%] text-slate-800 px-4 py-1 rounded-full'
                         />
 
-                        <button type="button" onClick={handleClick} className='btn-primary mr-4'>
+                        <button type="button" onClick={handleClick} className='btn-primary'>
                             Enter
                         </button>
 
