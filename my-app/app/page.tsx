4f9f64d413fbd942b51d964b7000ca347854da40
TLRKiliann <philogenie@protonmail.com>
2024-02-20 21:44:50 +0100

    select in a form submit created to access rooms (3)

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,17 +1,59 @@
-//import Image from "next/image";
-import Link from 'next/link';
+"use client";
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const router = useRouter();
+  const [selectVal, setSelectVal] = useState("security");
+
+  const handleSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.currentTarget;
+    console.log(value, "value selected");
+    setSelectVal(value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const finalVal = formData.get("room");
+    if (finalVal === "security") {
+      router.push(`/chatroom/security`);
+    } else if (finalVal === "development") {
+      router.push(`/chatroom/development`);
+    } else if (finalVal === "it") {
+      router.push(`/chatroom/it`);
+    } else {
+      console.log("Nothing choosen !")
+    }
+  }
+
   return (
     <main className="w-full min-h-screen">
 
-      <div className='flex items-center justify-content border'>
+      <div className='flex items-center justify-content'>
 
-        <Link href="/chatroom" className='link-btn'>Chat room</Link>
-
-        <Link href="/chatroom" className='link-btn'>Chat room 2</Link>
+        <h1 className='text-4xl font-bold p-8'>Welcome to Chatr00m !</h1>
 
       </div>
+
+      <hr />
+
+      <form onSubmit={(e) => handleSubmit(e)} className='flex flex-col items-center justify-between h-[120px] mt-40 p-4'>
+
+        <label htmlFor="room">Select a room:
+          <select name="room" id="room" value={selectVal} onChange={handleSelect} className='text-slate-800 mx-2 px-2 py-1'>
+            <option value="security">Security</option>
+            <option value="development">Development</option>
+            <option value="it">IT</option>
+          </select>
+        </label>
+
+        <button type="submit" className='btn-primary'>Submit</button>
+
+      </form>
 
     </main>
   );
