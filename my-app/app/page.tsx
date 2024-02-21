6954e6081e73e5da90852f2ca226a70098e7001b
TLRKiliann <philogenie@protonmail.com>
2024-02-21 21:28:49 +0100

    some temporary changes

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,14 @@
 import FormSelectRoom from "@/app/components/FormSelectRoom";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen">
+    <main className="w-full min-h-screen bg-slate-900">
 
       <div className='flex items-center justify-content'>
 
         <h1 className='text-4xl font-bold p-8'>Welcome to Chatr00m !</h1>
-
+        <Link href="/chatroom">Chatroom</Link>
       </div>
 
       <hr />
