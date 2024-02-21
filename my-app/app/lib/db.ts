1f3daa42d86878a89505541d940ad5b443f10676
TLRKiliann <philogenie@protonmail.com>
2024-02-21 14:09:27 +0100

    mysql query to call chatroom setup

---
+++ my-app/app/lib/db.ts
@@ -0,0 +1,38 @@
+import mysql from 'mysql2/promise';
+import type {
+    UsersChatProps,
+} from './definitions';
+
+type GenericProps = UsersChatProps | [];
+
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    port: Number(process.env.MYSQL_PORT),
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+const queryChatRoom = async (query: string, data: GenericProps): Promise<UsersChatProps[]> => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const [result] = await connection.execute(query, data);
+        return result as UsersChatProps[];
+    } catch (error) {
+        console.error(error);
+        throw error;
+    } finally {
+        if (connection) {
+            connection.release();
+        }
+    }
+};
+
+export {
+    queryChatRoom
+};
