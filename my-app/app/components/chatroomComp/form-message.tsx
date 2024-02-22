e72ac0faa42f382f66bb6a4c1f55591d90862899
TLRKiliann <philogenie@protonmail.com>
2024-02-22 10:08:31 +0100

    map data setup done

--- my-app/app/components/chatroomComp/form-message.tsx
+++ my-app/app/components/chatroomComp/form-message.tsx
@@ -9,14 +9,14 @@ import { redirect } from 'next/navigation';
 
 export default function FormMessage({data}: {data: UsersChatProps[]}) {
 
+    const { pending } = useFormStatus();
+    const [ code, formAction ] = useFormState(mysqlQueryChatroom, undefined)
+
     const {data: session} = useSession();
 
     if (!session) {
         redirect("/login")
     };
-
-    const { pending } = useFormStatus();
-    const [ code, formAction ] = useFormState(mysqlQueryChatroom, undefined)
 
     const [username, setUsername] = useState<string>("");
 
@@ -30,68 +30,43 @@ export default function FormMessage({data}: {data: UsersChatProps[]}) {
     const [message, setMessage] = useState<string>("");
 
     console.log("hello FormMessage");
-/*     //const username = session.user?.name;
-    //console.log(username, "username");
-   
 
-    //const derivatedState = useMemo(() => message, [message]);
-
-    //const id = Date.now();
-
-    //console.log(data, "data (2)")
-
-    const findById = data.find((d: UsersChatProps) => d.username === session.user.name);
-    
-    console.log(findById, "findById");
-    console.log("hello FormMessage (2)")
-    
-    let id;
-    let room;
-    let online;
-    let email;    
-
-    if (findById !== undefined && findById !== null) {
-        id = findById.id;
-        room = findById.room;
-        email = findById.email;
-        online = findById.online;
-    }
-
-    console.log(id, room, online, email, "definitvely");
- */
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = event.currentTarget;
         setMessage(value);
     };
 
     return (
-        <form action={formAction} className='absolute z-10 flex items-center justify-between w-[80%] h-[80px]
-            bg-gradient-to-r from-blue-900 from-10% via-sky-700 via-30% to-blue-900 to-90% 
-            px-4'
-        >
+        <>
+            {data.map((d: UsersChatProps) => (
+                username === d.username ? (
+                    <form action={formAction} className='absolute z-10 flex items-center justify-between w-[80%] h-[80px]
+                        bg-gradient-to-r from-blue-900 from-10% via-sky-700 via-30% to-blue-900 to-90% px-4'
+                    >
 
-            <input type="text" id="username" name="username" value={username} hidden readOnly />
-{/* 
-            <input type="number" id="id" name="id" value={id} hidden readOnly />
-            <input type="text" id="username" name="username" value={username} hidden readOnly />
-            <input type="text" id="email" name="email" value={email} hidden readOnly />
-            <input type="text" id="online" name="online" value={online} hidden readOnly />
- */}
-            <input type="text" id="message" name="message" value={message} onChange={handleChange} 
-                placeholder="Comment something here..." 
-                className='w-[90%] text-slate-800 placeholder:text-slate-500 px-4 py-1 rounded-full'
-            />
+                        <input type="number" id="id" name="id" value={d.id} hidden readOnly />
+                        <input type="text" id="username" name="username" value={username} hidden readOnly />
+                        <input type="text" id="email" name="email" value={d.email} hidden readOnly />
+                        <input type="number" id="online" name="online" value={d.online} hidden readOnly />
 
-{/*             <input type="text" id="room" name="room" value={room} hidden readOnly />
- */}
-            <button type="submit" id="submit" name="submit" value="btnSubmit" disabled={pending}
-                className='btn-primary shadow-light'>
-                {pending ? "Pending..." : "Enter"}
-            </button>
+                        <input type="text" id="message" name="message" value={message} onChange={handleChange} 
+                            placeholder="Comment something here..." 
+                            className='w-[90%] text-slate-800 placeholder:text-slate-500 px-4 py-1 rounded-full'
+                        />
 
-            {code?.message ? (
-                <p className='text-lg text-green-400'>{code.message}</p>
-            ) : null}
-        </form>
+                        <input type="text" id="room" name="room" value={d.room} hidden readOnly />
+
+                        <button type="submit" id="submit" name="submit" value="insert" disabled={pending}
+                            className='btn-primary shadow-light'>
+                            {pending ? "Pending..." : "Enter"}
+                        </button>
+
+                        {code?.message ? (
+                            <p className='text-lg text-green-400'>{code.message}</p>
+                        ) : null}
+                    </form>
+                ) : null
+            ))}
+        </>
     )
-}
+};
