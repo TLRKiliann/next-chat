e4aa305ed51771076a8743f69b96b92412fd2af9
TLRKiliann <philogenie@protonmail.com>
2024-02-22 16:56:54 +0100

    new config for msg and screen for msg

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -13,9 +13,13 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      dropShadow: {
+      },
       boxShadow: {
         'btn': '0px 0px 7px rgba(100, 100, 100, 1)',
+        'msg': '0px 0px 10px rgba(0, 0, 0, 0.2)',
         'light': '0px 0px 7px rgba(255, 255, 255, 0.9)',
+        'inside': 'inset 0px 0px 10px rgba(0, 0, 0, 1)',
       },
     },
   },
