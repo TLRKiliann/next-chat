3091f667d01c15ce3b8b56eb6c31af576439af73
TLRKiliann <philogenie@protonmail.com>
2024-02-23 09:42:43 +0100

    ui improved

--- my-app/app/components/chatroomComp/user-online.tsx
+++ my-app/app/components/chatroomComp/user-online.tsx
@@ -23,11 +23,11 @@ export default function UserOnline({dataroom}: {dataroom: UsersChatProps[]}) {
 
             {mapping.map((user: UsersChatProps) => (
                     <div key={user.id}
-                        className='flex items-center justify-start bg-slate-800 border-b border-slate-500 px-4 py-3'>
+                        className='flex items-center justify-start bg-slate-800 border-b border-slate-700 px-4 py-3'>
                         <Image src={user.img} width={30} height={30} alt={user.username} 
                             className='flex w-[30px] h-[30px] object-cover rounded-full'
                         />
-                        <p className='w-[80%] px-2'>{user.username}</p>
+                        <p className='w-[80%] text-lg pl-4'>{user.username}</p>
 
                         {user.online === 1 
                             ? <span className='w-[20px] h-[20px] bg-green-500 border border-green-500 rounded-full'></span> 
