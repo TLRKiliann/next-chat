d99a5ee7b1b06b2e271e508c79102d158ef09eed
TLRKiliann <philogenie@protonmail.com>
2024-02-24 15:22:09 +0100

    copy from chatroom page

--- my-app/app/chatroom/info/page.tsx
+++ my-app/app/chatroom/info/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { queryChatRoom, queryUsers } from '@/app/lib/db';
+import FormMessage from '@/app/components/chatroomComp/form-message';
+import ScreenMessage from '@/app/components/chatroomComp/screen-message';
+import UserOnline from '@/app/components/chatroomComp/user-online';
 
-export default function InfoRoom() {
+export default async function InfoRoom() {
+
+    const req = await queryUsers("SELECT * FROM userschat", []);
+    const dataUsers: string = JSON.stringify(req);
+
+    const request = await queryChatRoom("SELECT * FROM inforoom", []);
+    const data = JSON.stringify(request);
+
     return (
-        <div>
+        <div className='bg-slate-900'>
             <h1 className='text-2xl italic font-bold p-[20px]'>
                 Info
             </h1>
-      </div>
+            <div className='flex w-full h-[calc(100%-72px)]'>
+
+                <UserOnline dataUsers={JSON.parse(dataUsers)} />
+
+                <div className='w-full h-content shadow-inside
+                    bg-gradient-to-r from-green-200 from-10% to-blue-200 to-90%'>
+
+                    <ScreenMessage dataroom={JSON.parse(data)} />
+
+                    <FormMessage dataroom={JSON.parse(data)} />
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
+
--- my-app/app/chatroom/question/page.tsx
+++ my-app/app/chatroom/question/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { queryChatRoom, queryUsers } from '@/app/lib/db';
+import FormMessage from '@/app/components/chatroomComp/form-message';
+import ScreenMessage from '@/app/components/chatroomComp/screen-message';
+import UserOnline from '@/app/components/chatroomComp/user-online';
 
-export default function QuestionRoom() {
+export default async function QuestionRoom() {
+
+    const req = await queryUsers("SELECT * FROM userschat", []);
+    const dataUsers: string = JSON.stringify(req);
+
+    const request = await queryChatRoom("SELECT * FROM question", []);
+    const data = JSON.stringify(request);
+
     return (
-        <div>
+        <div className='w-full h-screen bg-slate-900'>
             <h1 className='text-2xl italic font-bold p-[20px]'>
                 Question
             </h1>
+
+            <div className='flex w-full h-[calc(100%-72px)]'>
+
+                <UserOnline dataUsers={JSON.parse(dataUsers)} />
+
+                <div className='w-full h-content shadow-inside
+                    bg-gradient-to-r from-green-200 from-10% to-blue-200 to-90%'>
+
+                    <ScreenMessage dataroom={JSON.parse(data)} />
+
+                    <FormMessage dataroom={JSON.parse(data)} />
+
+                </div>
+
+            </div>
+
         </div>
     )
 }
