b2923e41f23393e10d9f7a6d9761b094b12b1c37
TLRKiliann <philogenie@protonmail.com>
2024-02-27 08:20:56 +0100

    algo to push 2 users in same room done

--- my-app/app/components/chatroomComp/user-online.tsx
+++ my-app/app/components/chatroomComp/user-online.tsx
@@ -2,7 +2,6 @@
 
 import type { UsersProps } from '@/app/lib/definitions';
 import React, { useState, useEffect } from 'react';
-import { useSession } from 'next-auth/react';
 import DisplayInvitation from './invitation/display-invitation';
 import ResponseReceiver from './invitation/response-receiver';
 import UsersBoard from './invitation/users-board';
@@ -10,26 +9,17 @@ import { useRouter } from 'next/navigation';
 
 export default function UserOnline({dataUsers}: {dataUsers: UsersProps[]}) {
 
-    const { data: session } = useSession();
-
     const router = useRouter();
 
-    const [userName, setUserName] = useState<string>("");
     const [senderResponse, setSenderResponse] = useState<UsersProps | undefined>(undefined)
 
-    const mapping = dataUsers.filter((obj: {username: string}, index: number) => {
+    const mapping: UsersProps[] = dataUsers.filter((obj: {username: string}, index: number) => {
         return index === dataUsers.findIndex((o: {username: string}) => obj.username === o.username)
     });
 
     const [newMapping, setNewMapping] = useState<UsersProps[]>(mapping);
     const [acceptInvite, setAcceptInvite] = useState<boolean>(false);
     const [refuseInvite, setRefuseInvite] = useState<boolean>(false);
-
-    useEffect(() => {
-        if (session && session.user && session.user.name) {
-            setUserName(session.user.name)
-        }
-    }, []);
 
     const handleResponse = (findSender: string) => {
         const responseToSender = dataUsers.find((data: UsersProps) => data.username === findSender);
@@ -50,30 +40,22 @@ export default function UserOnline({dataUsers}: {dataUsers: UsersProps[]}) {
 
     const handleRouteToChange = () => {
         setTimeout(() => {
-            const filterUserSession = dataUsers.filter((d: UsersProps) => d.username === userName);
-            const senderMessage = filterUserSession[0]?.sender;
-
-            const mappingFilterSender = dataUsers.filter((n: UsersProps) => n.username === senderMessage);
-            const filterDataByRoomQuestion = dataUsers.filter((d: UsersProps) => d.selectedroom === "question");
-            const filterDataByRoomInfo = dataUsers.filter((d: UsersProps) => d.selectedroom === "info");
-            const filterDataByRoomSecret = dataUsers.filter((d: UsersProps) => d.selectedroom === "confidential");
-
-            if ((filterDataByRoomQuestion.length === 2) || (filterDataByRoomInfo.length === 2) || (filterDataByRoomSecret.length === 2)) {
-                if ((filterUserSession[0]?.selectedroom === "question") && (filterDataByRoomQuestion[0].selectedroom === "question")) {
+            const filterDataByRoom = dataUsers.filter((d: UsersProps) => d.selectedroom);
+            const filterDataByRoomFind = dataUsers.find((d: UsersProps) => d.selectedroom);
+            if (filterDataByRoom.length === 2) {
+                if (filterDataByRoomFind?.selectedroom === "question") {
                     router.push("/chatroom/question");
-                } else if ((filterUserSession[0]?.selectedroom === "info") && (filterDataByRoomSecret[0]?.selectedroom === "info")) {
+                } else if (filterDataByRoomFind?.selectedroom === "info") {
                     router.push("/chatroom/info");
-                } else if ((filterUserSession[0]?.selectedroom === "confidential") && (filterDataByRoomSecret[0]?.selectedroom === "confidential")) {
-                    //console.info(filterUserSession[0]?.selectedroom, "1 - is defined ???")
-                    //console.info(filterDataByRoomSecret[0]?.selectedroom, "2 - is defined ???")
+                } else if (filterDataByRoomFind?.selectedroom === "confidential") {
                     router.push("/chatroom/confidential");
                 } else {
                     console.log("2 users required !")
                 }
-            } else if ((filterDataByRoomQuestion.length > 2) && (filterDataByRoomInfo.length > 2) && (filterDataByRoomSecret.length > 2)) {
+            } else if (filterDataByRoom.length > 2) {
                 console.error("users greater than 2 !")
             } else {
-                console.error("2 users required !")
+                console.info("2 users required !")
             };
         }, 2000)
     };
