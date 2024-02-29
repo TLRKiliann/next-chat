7f3128331154c71d97bd7fc093ef189910842dce
TLRKiliann <philogenie@protonmail.com>
2024-02-29 10:40:18 +0100

    reusable components added

--- my-app/app/chatroom/programming/page.tsx
+++ my-app/app/chatroom/programming/page.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import type { Metadata } from 'next';
+import React from 'react';
+import { queryChatRoom, queryUsers } from '@/app/lib/db';
+import HeaderChatroom from '@/app/components/chatroomComp/header-chatroom';
+import FormMessage from '@/app/components/chatroomComp/form-message';
+import ScreenMessage from '@/app/components/chatroomComp/screen-message';
+import UserOnline from '@/app/components/chatroomComp/user-online';
+import BackToChatroom from '@/app/components/nextroom/backto-chatroom';
 
-export default function ProgrammingPage() {
-  return (
-    <div className='w-full h-screen bg-slate-900'>
-      <h1 className='text-2xl italic font-bold p-[20px]'>
-        Programming
-      </h1>
-    </div>
-  )
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Programming"
+    },
+    description: "Programming room"
+};
+
+export default async function ProgrammingRoom() {
+
+    const req = await queryUsers("SELECT * FROM userschat", []);
+    const dataUsers: string = JSON.stringify(req);
+
+    //Programming
+    const request = await queryChatRoom("SELECT * FROM question", []);
+    const data = JSON.stringify(request);
+
+    if (!dataUsers) {
+        throw new Error("Error: dataUsers not loaded for db");
+    };
+
+    if (!data) {
+        throw new Error("Error: data not loaded for db");
+    };
+
+    return (
+        <div className='w-full h-screen bg-slate-900'>
+
+            <HeaderChatroom>Programming</HeaderChatroom>
+
+            <div className='flex w-full h-[calc(100%-72px)]'>
+
+                <UserOnline dataUsers={JSON.parse(dataUsers)} />
+
+                <div className='w-full h-content shadow-inside bg-slate-50'>
+
+                    <ScreenMessage dataroom={JSON.parse(data)} />
+
+                    <FormMessage dataroom={JSON.parse(data)} dataUsers={JSON.parse(dataUsers)} />
+
+                </div>
+
+            </div>
+
+            <BackToChatroom dataUsers={JSON.parse(dataUsers)} />
+
+        </div>
+    )
 }
--- my-app/app/chatroom/security/page.tsx
+++ my-app/app/chatroom/security/page.tsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import type { Metadata } from 'next';
+import React from 'react';
+import { queryChatRoom, queryUsers } from '@/app/lib/db';
+import HeaderChatroom from '@/app/components/chatroomComp/header-chatroom';
+import FormMessage from '@/app/components/chatroomComp/form-message';
+import ScreenMessage from '@/app/components/chatroomComp/screen-message';
+import UserOnline from '@/app/components/chatroomComp/user-online';
+import BackToChatroom from '@/app/components/nextroom/backto-chatroom';
 
-export default function SecurityPage() {
-  return (
-    <div className='w-full h-screen bg-slate-900'>
-      <h1 className='text-2xl italic font-bold p-[20px]'>
-        Security
-      </h1>
-    </div>
-  )
+export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Security"
+    },
+    description: "Security room"
+};
+
+export default async function SecurityRoom() {
+
+    const req = await queryUsers("SELECT * FROM userschat", []);
+    const dataUsers: string = JSON.stringify(req);
+
+    //Security
+    const request = await queryChatRoom("SELECT * FROM question", []);
+    const data = JSON.stringify(request);
+
+    if (!dataUsers) {
+        throw new Error("Error: dataUsers not loaded for db");
+    };
+
+    if (!data) {
+        throw new Error("Error: data not loaded for db");
+    };
+
+    return (
+        <div className='w-full h-screen bg-slate-900'>
+
+            <HeaderChatroom>Security</HeaderChatroom>
+
+            <div className='flex w-full h-[calc(100%-72px)]'>
+
+                <UserOnline dataUsers={JSON.parse(dataUsers)} />
+
+                <div className='w-full h-content shadow-inside bg-red-400'>
+
+                    <ScreenMessage dataroom={JSON.parse(data)} />
+
+                    <FormMessage dataroom={JSON.parse(data)} dataUsers={JSON.parse(dataUsers)} />
+
+                </div>
+
+            </div>
+
+            <BackToChatroom dataUsers={JSON.parse(dataUsers)} />
+
+        </div>
+    )
 }
