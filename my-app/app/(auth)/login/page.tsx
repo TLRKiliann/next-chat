8b95a7570bc7ea275762ac25a9f5cb4719f787bd
TLRKiliann <philogenie@protonmail.com>
2024-03-01 12:31:09 +0100

    some changes with route for login

--- my-app/app/(auth)/login/page.tsx
+++ my-app/app/(auth)/login/page.tsx
@@ -29,7 +29,7 @@ export default function Login() {
       </div>
     )
   } else {
-    redirect("/chatroom")
+    redirect("/")
   }
 }
 
--- my-app/app/middleware.ts
+++ my-app/app/middleware.ts
@@ -2,13 +2,13 @@ import type { NextRequest } from 'next/server';
 import { redirect } from 'next/navigation';
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/chatroom')) {
+  if (request.nextUrl.pathname.startsWith('/')) {
     redirect('/login')
   }
-  if (request.nextUrl.pathname.startsWith('/about')) {
+  if (request.nextUrl.pathname.startsWith('/email')) {
     redirect('/login')
   }
 }
 export const config = {
-  matcher: ['/chatroom', '/about'],
+  matcher: ['/', '/email'],
 }
