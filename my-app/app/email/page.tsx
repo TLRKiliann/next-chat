009702e2985e66d40356c9290461318d4a5a5a21
TLRKiliann <philogenie@protonmail.com>
2024-03-01 14:27:56 +0100

    everything seems to be ok with email's query

--- my-app/app/email/page.tsx
+++ my-app/app/email/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import React from 'react';
-import { queryUsers } from '@/app/lib/db';
+import { queryEmail, queryUsers } from '@/app/lib/db';
 import HeaderChatroom from '../components/chatroomComp/header-chatroom';
 import EmailForm from '../components/emailComp/email-form';
 
@@ -16,16 +16,23 @@ export default async function EmailPage() {
     const req = await queryUsers("SELECT * FROM userschat", []);
     const dataUsers: string = JSON.stringify(req);
 
+    const requestEmail = await queryEmail("SELECT * FROM mailbox", []);
+    const emailResponse: string = JSON.stringify(requestEmail);
+
     if (!dataUsers) {
         throw new Error("Error: dataUsers not accessible from db !");
-    }
+    };
+
+    if (!emailResponse) {
+        throw new Error("Error: emailResponse not accessible from db !");
+    };
 
     return (
         <div className='w-full min-h-screen bg-slate-800'>
 
             <HeaderChatroom>Email</HeaderChatroom>
 
-            <EmailForm dataUsers={JSON.parse(dataUsers)} />
+            <EmailForm dataUsers={JSON.parse(dataUsers)} emailResponse={JSON.parse(emailResponse)} />
 
         </div>
     )
