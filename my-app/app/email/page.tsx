c578c616570bb463369e69856e7215dd2f8eeb6b
TLRKiliann <philogenie@protonmail.com>
2024-03-01 12:29:50 +0100

    setup done

--- my-app/app/email/page.tsx
+++ my-app/app/email/page.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import type { Metadata } from 'next';
+import React from 'react';
+import { queryUsers } from '@/app/lib/db';
+import HeaderChatroom from '../components/chatroomComp/header-chatroom';
+import EmailForm from '../components/emailComp/email-form';
 
-export default function EmailPage() {
+export const metadata: Metadata = {
+    title: {
+      absolute: "Email"
+    },
+    description: "Email page"
+};
+
+export default async function EmailPage() {
+
+    const req = await queryUsers("SELECT * FROM userschat", []);
+    const dataUsers: string = JSON.stringify(req);
+
+    if (!dataUsers) {
+        throw new Error("Error: dataUsers not accessible from db !");
+    }
+
     return (
-        <div>EmailPage</div>
+        <div className='w-full min-h-screen bg-slate-800'>
+
+            <HeaderChatroom>Email</HeaderChatroom>
+
+            <EmailForm dataUsers={JSON.parse(dataUsers)} />
+
+        </div>
     )
 };
