c4655161387ee331c563576cca5a9c070eb05854
TLRKiliann <philogenie@protonmail.com>
2024-03-03 14:07:19 +0100

    some changes

--- my-app/app/components/emailComp/email-form.tsx
+++ my-app/app/components/emailComp/email-form.tsx
@@ -37,21 +37,21 @@ export default function EmailForm({dataUsers, emailResponse}: {dataUsers: UsersP
       setEmail(value);
     };
 
-    const handleText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const handleText = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
         const { value }: HTMLTextAreaElement = event.currentTarget;
         setTextArea(value);
     };
 
     return (
-        <div className='w-[440px] h-auto bg-gradient-to-r from-blue-900 from-10% 
+        <div className='md:w-[440px] xl:w-[600px] h-auto bg-gradient-to-r from-blue-900 from-10% 
         via-sky-700 via-50% to-blue-900 to-90% rounded-xl'>
             
-            <form action={formData} className='flex flex-col items-center justify-around'>
+            <form action={formData} className='flex flex-col px-10'>
 
-                <div className='flex items-center justify-center w-full h-20 '>
+                <div className='flex items-center justify-between w-full py-4'>
                     <label htmlFor="email" className='text-lg'>Select an email:</label>
                     <select name="email" id="email" value={email} onChange={handleEmail} 
-                        className='text-slate-700 mx-2 px-2 py-1 rounded'>
+                        className='text-slate-700 px-2 py-1 rounded'>
                         {dataUsers.map((user: UsersProps) => (
                             <option key={user.id} value={user.email}>{user.email}</option>
                         ))}
@@ -60,22 +60,24 @@ export default function EmailForm({dataUsers, emailResponse}: {dataUsers: UsersP
 
                 <input type="number" name="id" id="id" value={newId} hidden readOnly />
                 <input type="text" name="sender" id="sender" value={userName} hidden readOnly />
-                {/* <input type="text" name="email" id="email" value={email} hidden readOnly /> */}
 
                 <textarea name="textArea" id="textArea" cols={30} rows={10} placeholder="Comment here..."
                     className='text-slate-700 p-2 rounded' value={textArea} onChange={handleText}
                 >
                 </textarea>
 
-                <div className='flex w-full h-20'>
-                    <button type="submit" id="submit" name="submit" value="btnEmail" disabled={pending} className='btn-primary m-auto'
+                <input type="number" name="bool_text" id="bool_text" value={0} hidden readOnly />
+
+                <div className='flex w-full py-4'>
+                    <button type="submit" id="submit" name="submit" value="btnEmail" disabled={pending} 
+                        className='font-bold btn-primary m-auto'
                     >
                         {pending ? "Pending..." : "Submit"}
                     </button>
                 </div>
 
                 {code?.message ? (
-                    <p className='text-slate-50 mb-4'>{code.message}</p>
+                    <p className='text-cyan-400 text-center mb-4'>{code.message}</p>
                 ) : null}
 
             </form>
