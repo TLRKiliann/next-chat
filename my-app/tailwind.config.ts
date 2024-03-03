d25b6775460cc8b19d9aefefb10afec53dc9249a
TLRKiliann <philogenie@protonmail.com>
2024-03-03 14:08:48 +0100

    new config for shadow btn created

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -17,6 +17,8 @@ const config: Config = {
       },
       boxShadow: {
         'btn': '0px 0px 7px rgba(100, 100, 100, 1)',
+        'btndark': '0px 0px 5px rgba(50, 50, 50, 0.4)',
+        'btndarkxl': '0px 0px 10px rgba(50, 50, 50, 0.4)',
         'msg': '0px 0px 10px rgba(0, 0, 0, 0.2)',
         'light': '0px 0px 7px rgba(255, 255, 255, 0.9)',
         'inside': 'inset 0px 0px 10px rgba(0, 0, 0, 1)',
